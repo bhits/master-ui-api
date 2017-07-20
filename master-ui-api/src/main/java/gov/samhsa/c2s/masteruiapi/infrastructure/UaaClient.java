d0dd89e2c467f1e5f082ec30ebea76711da21c3c
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-07-20 15:46:28 -0400

    Implement Feign client to call UAA during login
    #14592 - Single UI 1 : As a developer I will create a single UI interface to login

---
+++ master-ui-api/src/main/java/gov/samhsa/c2s/masteruiapi/infrastructure/UaaClient.java
@@ -0,0 +1,25 @@
+package gov.samhsa.c2s.masteruiapi.infrastructure;
+
+import feign.Headers;
+import gov.samhsa.c2s.masteruiapi.config.CoreFeignConfiguration;
+import gov.samhsa.c2s.masteruiapi.service.dto.UaaTokenDto;
+import gov.samhsa.c2s.masteruiapi.service.dto.UaaUserInfoDto;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+@FeignClient(name = "uaa", url = "http://localhost", path = "/uaa", configuration = CoreFeignConfiguration.class)
+public interface UaaClient {
+
+    @RequestMapping(value = "/oauth/token", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @Headers("Content-Type: application/x-www-form-urlencoded")
+    UaaTokenDto getTokenUsingPasswordGrant(Map<String, ?> formParams);
+
+
+    @RequestMapping(value = "/userinfo", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    UaaUserInfoDto getUserInfo(@RequestHeader("Authorization") String authorizationHeader);
+}
