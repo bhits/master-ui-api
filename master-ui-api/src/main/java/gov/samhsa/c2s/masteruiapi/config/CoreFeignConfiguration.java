0cb46b7376664948b78def776ce4da99359e6109
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-08-16 11:06:42 -0400

    Reformat code and remove @Configuration annotation
    #14794 - Single UI 4: As a developer I will create a notification to user after 3 failed login attempts about account lockout.

--- master-ui-api/src/main/java/gov/samhsa/c2s/masteruiapi/config/CoreFeignConfiguration.java
+++ master-ui-api/src/main/java/gov/samhsa/c2s/masteruiapi/config/CoreFeignConfiguration.java
@@ -7,11 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.cloud.netflix.feign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
-@Configuration
 public class CoreFeignConfiguration {
 
     private static final String PROTOTYPE = "prototype";
